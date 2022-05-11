7c946ef042c070a66361cc2c2a6b8f75c2b157de
Jack <jacopo.derosa00@gmail.com>
2022-05-11 22:00:20 +0200

    Started scene management

--- Assets/Scripts/UI/Main Menu/MainMenuController.cs
+++ Assets/Scripts/UI/Main Menu/MainMenuController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenuController : MonoBehaviour
 {
@@ -7,6 +9,8 @@ public class MainMenuController : MonoBehaviour
     [SerializeField] private GameObject _mainMenuCam;
     [SerializeField] private GameObject _buttons;
     [SerializeField] private GameObject _logo;
+    [SerializeField] private GameObject _loadingScreen;
+    [SerializeField] private TMP_Text _loadingText;
 
     public void GoToSquadEditor()
     {
@@ -17,11 +21,21 @@ public class MainMenuController : MonoBehaviour
         _squadEditorCam.SetActive(true);
         _mainMenuCam.SetActive(false);
     }
-
     public void GoToMainMenu()
     {
         if (_mainMenuCam.activeInHierarchy) return;
         _squadEditorCam.SetActive(false);
         _mainMenuCam.SetActive(true);
+    }
+
+    public void EnterQueue()
+    {
+        StartCoroutine(QueueUp());
+    }
+
+    private IEnumerator QueueUp()
+    {
+        _loadingScreen.SetActive(true);
+        yield return SceneManager.LoadSceneAsync(1);
     }
 }
