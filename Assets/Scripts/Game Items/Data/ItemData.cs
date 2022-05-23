b4776ffa775253a3803c73fbf9541912de01bcc5
Jack <jacopo.derosa00@gmail.com>
2022-05-23 07:15:15 +0200

    Added sprite to game item

--- Assets/Scripts/Game Items/Data/ItemData.cs
+++ Assets/Scripts/Game Items/Data/ItemData.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
 
-
-
 public class ItemData : ScriptableObject
 {
+    [SerializeField] private Sprite _sprite;
     [SerializeField] private int _cost;
     [SerializeField] private ItemRarity _rarity;
     [SerializeField] private int _reuqiredLevel;
     [SerializeField] private Factions _enabledFactions;
 
+    public Sprite Sprite { get => _sprite; }
     public int Cost { get => _cost; }
     public ItemRarity Rarity { get => _rarity; }
     public int RequiredLevel { get => _reuqiredLevel; }
